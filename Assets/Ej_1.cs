83dfad01478576712eb670fb470e838cfe3d4fba
mateopuli <101335430+mateopuli@users.noreply.github.com>
2022-04-11 11:27:52 -0300

    ej1inprogress

---
+++ Assets/Ej_1.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej_1 : MonoBehaviour
+{
+    public string codigo;
+    public int estadia;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int pesoalimentoG;
+        pesoalimentoG = estadia * 300;
+        int pesoalimentoP;
+        pesoalimentoP = estadia * 400;
+        int pesoalimentoPG;
+        pesoalimentoPG = estadia * 700;
+
+        if (codigo == "G")
+        {
+            Debug.Log("Para ese periodo se necesitan " + pesoalimentoG + " gramos de alimento");
+        }
+        else if (codigo == "P")
+        {
+            Debug.Log("Para ese periodo se necesitan " + pesoalimentoP + " gramos de alimento");
+        }
+        else if (codigo == "PG")
+        {
+            Debug.Log("Para ese periodo se necesitan " + pesoalimentoPG + " gramos de alimento");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
