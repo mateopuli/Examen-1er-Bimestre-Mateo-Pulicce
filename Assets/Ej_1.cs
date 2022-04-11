52f102a308add685bdc921ab978bcde4cf1bd26a
mateopuli <101335430+mateopuli@users.noreply.github.com>
2022-04-11 11:34:15 -0300

    ej1almostfinished

--- Assets/Ej_1.cs
+++ Assets/Ej_1.cs
@@ -16,17 +16,26 @@ public class Ej_1 : MonoBehaviour
         int pesoalimentoPG;
         pesoalimentoPG = estadia * 700;
 
+        int precioalimentoG;
+        precioalimentoG = pesoalimentoG * 80;
+        int precioalimentoP = pesoalimentoP * 80;
+        int precioalimentoPG;
+        precioalimentoPG = pesoalimentoPG * 80;
+
         if (codigo == "G")
         {
             Debug.Log("Para ese periodo se necesitan " + pesoalimentoG + " gramos de alimento");
+            Debug.Log("El costo del alimento es de " +precioalimentoG+ " pesos");
         }
         else if (codigo == "P")
         {
             Debug.Log("Para ese periodo se necesitan " + pesoalimentoP + " gramos de alimento");
+            Debug.Log("El costo del alimento es de " + precioalimentoP + " pesos");
         }
         else if (codigo == "PG")
         {
             Debug.Log("Para ese periodo se necesitan " + pesoalimentoPG + " gramos de alimento");
+            Debug.Log("El costo del alimento es de " + precioalimentoPG + " pesos");
         }
     }
 
